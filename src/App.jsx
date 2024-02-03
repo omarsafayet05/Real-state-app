aff1241b90382bda2dce95fea7a9da156f47c5fe
omarsafayet05 <safayetkhan4431@gmail.com>
2024-02-03 20:43:57 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+//import { useState } from "react";
+
+function App() {
+  //comma Operator
+  let x = 11;
+  x = (x++, x);
+  console.log(x);
+
+  let y = (5 + 4, 3 + 4);
+  console.log(y);
+
+  let z = (5, 5) + (6, 9);
+  console.log(z);
+  return (
+    <>
+      <h2 className="text-cyan-500 flex justify-center">Hello Brother</h2>
+    </>
+  );
+}
+
+export default App;
